5fbee959f735704a94dae922353e1357ffa59383
Elverth Pulido <alexth117@hotmail.com>
2023-01-16 10:27:11 -0500

    App Torneos

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Banner/>
     </View>
   );
 }
-
+const Banner = () => {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Image
+      source={require('./assets/logo.png')}
+      style={{ width: 200, height: 200 }}
+    />
+  </View>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
